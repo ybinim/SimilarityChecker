6fc90ce0a16bf915155d5141d510565da578f2f8
ImYongbin <lyb0203@gmail.com>
2025-03-28 10:43:22 +0900

    [feature/refactoring] similaritychecker_length

--- Project25/similarityChecker.cpp
+++ Project25/similarityChecker.cpp
@@ -2,11 +2,29 @@
 #include <string>
 using namespace std;
 
+const int MAX_POINT_OF_LENGTH_SIMILARITY = 60;
+
 class SimilarityChecker {
 public:
-	int checkLength(vector<string> str) {
-		if (str[0] == "A" && str[1] == "A") {
-			return 60;
-		}
+	vector<int> getLength(const vector<string>& str) {
+		vector<int>ret;
+		ret.push_back(str[0].size());
+		ret.push_back(str[1].size());
+		return ret;
+	}
+
+	int getLenthPoint(const vector<string>& str) {
+		double ret;
+		double max, min;
+		vector<int>value = getLength(str);
+
+		max = (value[0] > value[1]) ? value[0] : value[1];
+		min = (value[0] < value[1]) ? value[0] : value[1];
+
+		if (max == min)	return MAX_POINT_OF_LENGTH_SIMILARITY;
+		if (max >= min * 2)	return 0;
+
+		ret = (1 - ((max - min) / min)) * MAX_POINT_OF_LENGTH_SIMILARITY;
+		return (int)ret;
 	}
 };
--- Project25/similiarityChecker_test.cpp
+++ Project25/similiarityChecker_test.cpp
@@ -9,10 +9,49 @@ TEST(tc1, tc2) {
 	SimilarityChecker sc;
 	vector<string>input = { "A", "A" };
 	int expected = 60;
-	int actual = sc.checkLength(input);
+	int actual = sc.getLenthPoint(input);
 	EXPECT_EQ(actual, expected);
 }
 
+TEST(tc1, 글자수리턴) {
+	SimilarityChecker sc;
+	vector<string>input = { "ABC", "AD" };
+	vector<int>expected = { 3, 2 };
+	vector<int> actual = sc.getLength(input);
+	EXPECT_EQ(actual, expected);
+}
+
+TEST(tc1, 글자수2배이상차이) {
+	SimilarityChecker sc;
+	vector<string>input = { "A", "BB" };
+	int expected = 0;
+	int actual = sc.getLenthPoint(input);
+	EXPECT_EQ(actual, expected);
+}
+
+TEST(tc1, 글자수일치) {
+	SimilarityChecker sc;
+	vector<string>input = { "ABC", "ABC" };
+	int expected = 60;
+	int actual = sc.getLenthPoint(input);
+	EXPECT_EQ(actual, expected);
+}
+
+TEST(tc1, 글자수부분일치) {
+	SimilarityChecker sc;
+	vector<string>input = { "AAABB", "BAA" };
+	int expected = 20;
+	int actual = sc.getLenthPoint(input);
+	EXPECT_EQ(actual, expected);
+}
+
+TEST(tc1, 글자수부분일치2) {
+	SimilarityChecker sc;
+	vector<string>input = { "AA", "AAE" };
+	int expected = 30;
+	int actual = sc.getLenthPoint(input);
+	EXPECT_EQ(actual, expected);
+}
 
 int main() {
 	::testing::InitGoogleMock();
