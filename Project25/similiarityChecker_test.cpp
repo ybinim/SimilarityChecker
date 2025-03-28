b645a1cf171f7d4e093b2e0af04e86cc4d15b603
ImYongbin <lyb0203@gmail.com>
2025-03-28 10:59:17 +0900

    [refactoring] test fixture 추가-반복코드 제거

--- Project25/similiarityChecker_test.cpp
+++ Project25/similiarityChecker_test.cpp
@@ -5,49 +5,42 @@
 
 using namespace std;
 
-TEST(tc1, tc2) {
+class SimilarityCheckerFixture : public ::testing::Test {
+public:
 	SimilarityChecker sc;
-	vector<string>input = { "A", "A" };
-	int expected = 60;
-	int actual = sc.getLenthPoint(input);
-	EXPECT_EQ(actual, expected);
-}
+	vector<string>input;
+};
 
-TEST(tc1, 글자수리턴) {
-	SimilarityChecker sc;
-	vector<string>input = { "ABC", "AD" };
+TEST_F(SimilarityCheckerFixture, TestGetLength) {
+	input = { "ABC", "AD" };
 	vector<int>expected = { 3, 2 };
 	vector<int> actual = sc.getLength(input);
 	EXPECT_EQ(actual, expected);
 }
 
-TEST(tc1, 글자수2배이상차이) {
-	SimilarityChecker sc;
-	vector<string>input = { "A", "BB" };
+TEST_F(SimilarityCheckerFixture, TestCheckLengDiff2times) {
+	input = { "A", "BB" };
 	int expected = 0;
 	int actual = sc.getLenthPoint(input);
 	EXPECT_EQ(actual, expected);
 }
 
-TEST(tc1, 글자수일치) {
-	SimilarityChecker sc;
-	vector<string>input = { "ABC", "ABC" };
+TEST_F(SimilarityCheckerFixture, TestGetMaxPoint) {
+	input = { "ABC", "ABC" };
 	int expected = 60;
 	int actual = sc.getLenthPoint(input);
 	EXPECT_EQ(actual, expected);
 }
 
-TEST(tc1, 글자수부분일치) {
-	SimilarityChecker sc;
-	vector<string>input = { "AAABB", "BAA" };
+TEST_F(SimilarityCheckerFixture, TestGetParitialPoint) {
+	input = { "AAABB", "BAA" };
 	int expected = 20;
 	int actual = sc.getLenthPoint(input);
 	EXPECT_EQ(actual, expected);
 }
 
-TEST(tc1, 글자수부분일치2) {
-	SimilarityChecker sc;
-	vector<string>input = { "AA", "AAE" };
+TEST_F(SimilarityCheckerFixture, 글자수부분일치2) {
+	input = { "AA", "AAE" };
 	int expected = 30;
 	int actual = sc.getLenthPoint(input);
 	EXPECT_EQ(actual, expected);
